d018beb9b90f4a703be07a922cff2d332179c0bb
alextech303 <alextech303@gmail.com>
2023-08-07 11:13:09 +0300

    correct1_function_5

--- Lab_05/tasks.cpp
+++ Lab_05/tasks.cpp
@@ -75,23 +75,54 @@ void swap3(int &x, int &y) {
 int function_4(int *array,int size) {
 	
 	int *pa = NULL;
-	//cout << "array[1] = " << array[1] << endl;
-	for (int i = 0; i < size; i++) {
+	
+	for (int i = size; i > 0 ; i--) {
 
 		
 		if (array[i]<0) {
 			int x = array[i];
 			pa = &x;
-			cout << "pa1 внутри функции = " << *pa << endl;
 		}
-		/*if (array[i] < 0) {
-			int x = array[i];
-			int* pa = &x;
-			cout << "pa1 внутри функции = " << pa << endl;
-		}*/
+		
 	}
 	return *pa;
 
+}
+
+int* function_5(int** array, int size) {
+	int count = 0;
+	int *pmin=new int [size];
+
+	for (int i = 0; i < size; i++) {
+
+		for (int j = 0; j < size; j++) {
+			if (array[i][j] % 2 == 0) {
+				count++;
+			}
+			cout << array[i][j] << "\t";
+			
+		}
+		cout << endl;
+	}
+	int* parr = new int[count];
+	int k = 0;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (array[i][j] % 2 == 0) {
+				parr[k] = array[i][j];
+				k++;
+			}
+
+		}
+	}
+	cout << "______________" << endl;
+	cout << "массив из четных чисел :" << endl;
+	for (int i = 0; i < count; i++) {
+		cout << *parr++ << "\t";
+	}
+	cout << endl;
+	cout << "________________" << endl;
+	return parr;
 }
 
 int main() {
@@ -130,14 +161,71 @@ int main() {
 	//swap3(x, y);
 
 	//function_4:
-	int array[10] = { 2,5,3,4,5,6,7,8,9,10 };
-	int pa1 = function_4(array, 10);
-	cout << "pa1 = " << pa1 << endl;
-	
-	
+	/*int array[10] = {-2,-5,3,4,5,6,7,8,9,10 };
+	int pa = function_4(array, 10);
+	cout << "pa = " << pa << endl;
+	*/
+	/*cout << "Введите количество указателей на одномерные массивы : " << endl;
+	int j;
+	cin >> j;*/
+	/*int z = 11;
+	int*  pz = &z;
+	int** ppz = &pz;
+	int*** pppz = &ppz;
+	cout << "z = " << z << endl;
+	cout << "pz = " << pz << endl;
+	cout << "ppz = " << ppz << endl;
+	cout << "pppz = " << pppz << endl;
+	cout << " *pz = " << *pz << endl;
+	cout << "**ppz = " << **ppz << endl;
+	cout << "***pppz = " << ***pppz << endl;
+
+	pz = new int[j];
+	for (int i = 0; i < j; i++) {
+		cout << "Введите " << i << " элемент массива" << endl;
+		cin >> pz[i];
+
+	}
+	cout << "_______________" << endl;
+	for (int i = 0; i < j; i++) {
+
+		cout << pz[i] << endl;;
+
+	}*/
+	/*При формировании двумерного динамического массива сначала выделяется 
+		память для массива указателей на одномерные массивы, 
+		а затем в цикле с параметром выделяется память под одномерные массивы.*/
+		
+	/*int** array2;
+	array2 = new int* [j];
+	for (int i = 0; i < j; i++) {
+		array2[i] = new int[10];
+	}
+
+	int** array22;
+	array22 = array2;
+	for (int i = 0; i < j; i++) {
+		for (int j = 0; j < 10; j++) {
+			array22[i][j] = i * 100 + j;
+		}
+	}
+
+	for (int i = 0; i < j; i++) {
+		cout << "\n";
+		for (int j = 0; j < 10; j++) {
+			cout<<array22[i][j]<<"\t";
+		}
+	}
+	*/
+	int** ar = new int*	[3];
+	ar[0] = new int[5] {1,2,3,4,5};
+	ar[1] = new int[5] {6,7,8,9,10};
+	ar[2] = new int[5] {11,12,13,14,15};
 	
 
+	cout << function_5(ar, 3) << endl;;
 
+	
 	system("pause");
 	return 0;
 
